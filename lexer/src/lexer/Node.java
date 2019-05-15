0266e0fd07e0b8dc61f90e8cfe798348f98dab04
Maynkun <33196063+Maynkun@users.noreply.github.com>
2019-05-15 23:30:40 +0300

    Create Node.java

---
+++ lexer/src/lexer/Node.java
@@ -0,0 +1,15 @@
+package lexer;
+
+public class Node {
+
+    Node next;
+    Node prev;
+    int  value;
+
+    public Node (Node next, Node prev, int value )
+    {
+        this.next=next;
+        this.prev=prev;
+        this.value = value;
+    }
+}

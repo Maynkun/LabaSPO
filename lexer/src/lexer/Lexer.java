655807e2b7effda24ad0bc310f81c7bcbe05ed29
Maynkun <33196063+Maynkun@users.noreply.github.com>
2019-05-20 22:49:29 +0300

    Update Lexer.java

--- lexer/src/lexer/Lexer.java
+++ lexer/src/lexer/Lexer.java
@@ -49,57 +49,69 @@ class LexerClass
                 {
                     ListOfLexems KeyWordsIf = new ListOfLexems(words[j],"KeyWordIf");
                     program.add(KeyWordsIf);
-                    //System.out.println(words[j]+"  KeyWord");
                 }
                 else if (Pattern.matches("^List$",words[j]))
                 {
                     ListOfLexems KeyWordList = new ListOfLexems(words[j],"KeyWordList");
                     program.add(KeyWordList);
-                    //	System.out.println(words[j]+"  Equally");
                 }
                 else if (Pattern.matches("^remove$",words[j]))
                 {
                     ListOfLexems KeyWordRemove = new ListOfLexems(words[j],"KeyWordRemove");
                     program.add(KeyWordRemove);
-                    //	System.out.println(words[j]+"  Equally");
                 }
                 else if (Pattern.matches("^Add$",words[j]))
                 {
                     ListOfLexems KeyWordAdd = new ListOfLexems(words[j],"KeyWordAdd");
                     program.add(KeyWordAdd);
-                    //	System.out.println(words[j]+"  Equally");
                 }
                 else if (Pattern.matches("^getElement$",words[j]))
                 {
                     ListOfLexems KeyWordGetElement = new ListOfLexems(words[j],"KeyWordGetElement");
                     program.add(KeyWordGetElement);
-                    //	System.out.println(words[j]+"  Equally");
                 }
                 else if (Pattern.matches("^getSize$",words[j]))
                 {
                     ListOfLexems KeyWordGetSize = new ListOfLexems(words[j],"KeyWordGetSize");
                     program.add(KeyWordGetSize);
-                    //	System.out.println(words[j]+"  Equally");
                 }
 
                 else if (Pattern.matches("^while$",words[j]))
                 {
                     ListOfLexems KeyWordWhile = new ListOfLexems(words[j],"KeyWordWhile");
                     program.add(KeyWordWhile);
-                    //	System.out.println(words[j]+"  Equally");
+                }
+
+                else if (Pattern.matches("^HashAdd$",words[j]))
+                {
+                    ListOfLexems KeyWordHashAdd = new ListOfLexems(words[j],"KeyWordHashAdd");
+                    program.add(KeyWordHashAdd);
+                }
+                else if (Pattern.matches("^HashRemove$",words[j]))
+                {
+                    ListOfLexems KeyWordHashRemove = new ListOfLexems(words[j],"KeyWordHashRemove");
+                    program.add(KeyWordHashRemove);
+                }
+                else if (Pattern.matches("^HashContain$",words[j]))
+                {
+                    ListOfLexems KeyWordHashContain = new ListOfLexems(words[j],"KeyWordHashContain");
+                    program.add(KeyWordHashContain);
+                }
+                else if (Pattern.matches("^HashSet$",words[j]))
+                {
+                    ListOfLexems KeyWordHashSet = new ListOfLexems(words[j],"KeyWordHashSet");
+                    program.add(KeyWordHashSet);
                 }
 
                 else if (Pattern.matches("^for$",words[j]))
                 {
                     ListOfLexems KeyWordFor = new ListOfLexems(words[j],"KeyWordFor");
                     program.add(KeyWordFor);
-                    //System.out.println(words[j]+"  Equally");
                 }
                 else if (Pattern.matches("^else$",words[j]))
                 {
                     ListOfLexems KeyWordElse = new ListOfLexems(words[j],"KeyWordElse");
                     program.add(KeyWordElse);
-                    //System.out.println(words[j]+"  Equally");
                 }
                 else for (int k =0; k<words[j].length();k++) {
                         char[] chars = words[j].toCharArray();
@@ -111,92 +123,78 @@ class LexerClass
                         {
                             ListOfLexems Semicolon = new ListOfLexems(Sem,"Semicolon");
                             program.add(Semicolon);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]=='+'|chars[k]=='-'|chars[k]=='*'|chars[k]=='/')
                         {
                             ListOfLexems Operation = new ListOfLexems(Sem,"Operation");
                             program.add(Operation);
-                            //	System.out.println(words[j]+"  Operation");
                         }
 
                         else if (chars[k]=='>'|chars[k]=='<'|Pattern.matches("!=|==|>=|<=",words[j])) /// доисправить
                         {
                             ListOfLexems booleanOP = new ListOfLexems(Sem,"booleanOP");
                             program.add(booleanOP);
-                            //	System.out.println(words[j]+"  Operation");
                         }
 
                         else if (chars[k]==':')
                         {
                             ListOfLexems Colon = new ListOfLexems(Sem,"Colon");
                             program.add(Colon);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]=='{')
                         {
                             ListOfLexems Open_brace = new ListOfLexems(Sem,"Open_brace");
                             program.add(Open_brace);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]=='}')
                         {
                             ListOfLexems Close_brace = new ListOfLexems(Sem,"Close_brace");
                             program.add(Close_brace);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]==',')
                         {
                             ListOfLexems Comma = new ListOfLexems(Sem,"Comma");
                             program.add(Comma);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]=='(')
                         {
                             ListOfLexems Open_bracket = new ListOfLexems(Sem,"Open_bracket");
                             program.add(Open_bracket);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]==')')
                         {
                             ListOfLexems Close_bracket = new ListOfLexems(Sem,"Close_bracket");
                             program.add(Close_bracket);
-                            //	System.out.println(words[j]+"  Equally");
                         }
 
                         else if (chars[k]=='=')
                         {
                             ListOfLexems Equally = new ListOfLexems(Sem,"Equally");
                             program.add(Equally);
-                            //	System.out.println(words[j]+"  Equally");
                         }
-
-
-                        else if (Character.isDigit(chars[k]))  // доработать
+                        
+                        else if (Character.isDigit(chars[k])) 
                         {
                             ListOfLexems Number = new ListOfLexems(Sem,"Number");
                             program.add(Number);
-                            //	System.out.println(words[j]+"  Number");
                         }
 
-                        else if (Character.isLetter(chars[k])) // доработать
+                        else if (Character.isLetter(chars[k]))
                         {
                             ListOfLexems Var = new ListOfLexems(Sem,"Var");
                             program.add(Var);
-                            //System.out.println(words[j]+"  Var");
                         }
 
                         else if (Pattern.matches(".+",words[j]))
                         {
                             ListOfLexems Error = new ListOfLexems(words[j],"Error");
                             program.add(Error);
-                            //System.out.println(words[j]+"  Error!");
                         }
                     }
             }
@@ -210,19 +208,15 @@ class LexerClass
 
         while(i<program.size())
         {
-            //System.out.println(i);
-
             if(i<program.size())
                 if(program.get(i).value.equals("Var"))
                 {
                     String Krya = "";
                     while(program.size()>i&program.get(i).value.equals("Var"))
                     {
-                        //System.out.println(program.get(i).lexem);
                         Krya += program.get(i).lexem;
                         i++;
                     }
-//				System.out.println(Krya);
                     ListOfLexems Var = new ListOfLexems(Krya,"Var");
                     massivOfLexems.add(Var);
                 } else if(program.get(i).value.equals("Number"))
@@ -234,14 +228,12 @@ class LexerClass
                         Krya += program.get(i).lexem;
                         i++;
                     }
-//					System.out.println(Krya);
                     ListOfLexems Number = new ListOfLexems(Krya,"Number");
                     massivOfLexems.add(Number);
                 } else
                 {
                     ListOfLexems Other = new ListOfLexems(program.get(i).lexem,program.get(i).value);
                     massivOfLexems.add(Other);
-                    //System.out.println(program.get(i).lexem);
                     i++;
 
                 }
@@ -275,27 +267,12 @@ public class Lexer
         boolean parser;
         LexerClass MetodLexer = new LexerClass ();
         ArrayList<ListOfLexems> program = new ArrayList<ListOfLexems>();
-        Poliz object = new Poliz(program);
-        ArrayList<String> poliz = new ArrayList<String>();
         try {program = MetodLexer.lexemSintex();}
         catch (Exception e) {e.printStackTrace();}
         Parser pars = new Parser(program);
         parser = pars.lang(program);
 		Poliz object1 = new Poliz(program);
-//      System.out.println(parser);
         if (parser)
 		object1.Polish();
-/*
-    List a = new List ();
 
-    System.out.println(a.getSize());
-    a.addBack(1);
-    a.addBack(2);
-    a.addBack(4);
-        System.out.println(a.getSize());
-
-        System.out.println(a.removeBack());
-        System.out.println(a.getSize());
-        System.out.println(a.getElement(2)); */
     }
-}
